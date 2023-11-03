0118c84ebe6ff2d653fe94def9b3832295c224b5
unknown <sameeksha.21ai041@sode-edu.in>
2023-11-03 10:24:41 +0530

    first commit

---
+++ src/components/Resume/Resume.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Resume() {
+  return (
+    <div>
+        <form className="App-cs">
+        <h1 className="App-hi"  >RESUME</h1>
+        <div className="App-rm" >
+        <h3>Sameeksha Nayak</h3>
+           <h3>sameekshasnayak@gmail.com</h3>
+           <h3>9980078320</h3>
+           <h3>Github:</h3>
+           <a href="https://github.com/Sameeksha226" target="_blank">https://github.com/Sameeksha226</a>
+           <div>
+            <h2>Skills:</h2>
+            <p>Basics in C <br></br>C++ <br></br>Java<br></br> Python<br></br> HTML<br></br> CSS</p>
+            <p>Communication skill</p>
+           </div>
+        </div>
+        <div className='App-jk'>
+        <h2>Personal Profile:</h2>
+        <p>I am a qualified web developer who is looking for job in order to enhance skills and gain more knowledge.<br></br>I have strong creative and analytical skills.</p>
+        </div>
+        <div className='App-jk'>
+        <p></p>
+        <h2>Educational  Qualification:</h2>
+        <p>St.Lawrence English Medium High School,Moodubelle <br></br> 2009-2019</p>
+        <p>Jnanaganga PU College,Nellikatte,Moodubelle <br></br> 2019-2021</p>
+        <p>Shri Madhwa Vadiraja Institute of Technology and Management,Bantakal <br></br> 2021-</p>
+        </div>
+        <div className='App-jk'>
+        <h2>Certificates and Achievements:</h2>
+        <p>Participated in intra-college mini-project competition:<br></br> 1.2022-Wall-e rover using arduino <br></br> 2.2023-Personal Assisstant</p>
+        <p>Participated in IEEE EUREKA 2022 & 2023</p>
+        <p>Participated in Ethical Hacking and Cybersecurity workshop by NICC India</p>
+        <p>Participated in Basics of Artificial Intelligence ,Machine Learning and Data Science workshop</p>
+        <p>Completed intra-internship at college-2022</p>
+    </div>
+        </form>
+    </div>
+  );
+}
+
+export default Resume
